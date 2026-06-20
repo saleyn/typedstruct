a5939bb210619cd9c362b87094ee343c55494ec3
Serge Aleynikov <saleyn@gmail.com>
2026-06-19 21:45:27 -0400

    Match length to the default length set by Elixir formatter (issue #3)

--- .formatter.exs
+++ .formatter.exs
@@ -5,7 +5,7 @@ locals_without_parens = [field: 2, field: 3, plugin: 1, plugin: 2]
     "{mix,.iex,.formatter,.credo}.exs",
     "{config,lib,test}/**/*.{ex,exs}"
   ],
-  line_length: 80,
+  line_length: 98,
   locals_without_parens: locals_without_parens,
   export: [locals_without_parens: locals_without_parens]
 ]
